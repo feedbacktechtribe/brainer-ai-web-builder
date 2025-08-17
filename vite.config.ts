385dfc50a99f5e253a93732f9dfbd9736ac763a2
feedbacktechtribe <feedback.techtribe@gmail.com>
2025-08-17 17:51:30 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/brainer-ai-web/',  // 👈 replace with your repo name
+  base: '/brainer-ai-web-builder/',  // 👈 replace with your repo name
 })
